9d71dc707d810be8a68c579bdd92882cb44625c1
PYee1999 <prestonkyee@gmail.com>
2024-04-13 18:00:18 -0400

    BART implementation; need testing (WIP)

--- models/bart.py
+++ models/bart.py
@@ -2,3 +2,13 @@
 # https://huggingface.co/facebook/bart-large
 # https://huggingface.co/docs/transformers/en/model_doc/bart
 
+from transformers import BartForConditionalGeneration, BartTokenizer
+
+model = BartForConditionalGeneration.from_pretrained("facebook/bart-large", forced_bos_token_id=0)
+tok = BartTokenizer.from_pretrained("facebook/bart-large")
+example_english_phrase = "UN Chief Says There Is No <mask> in Syria"
+batch = tok(example_english_phrase, return_tensors="pt")
+generated_ids = model.generate(batch["input_ids"])
+assert tok.batch_decode(generated_ids, skip_special_tokens=True) == [
+    "UN Chief Says There Is No Plan to Stop Chemical Weapons in Syria"
+]
