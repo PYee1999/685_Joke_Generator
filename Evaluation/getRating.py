944936475934794eac43304618f0c3b3680f48e8
Rigved <rmanoj@umass.edu>
2024-05-12 15:33:24 -0400

    update evaluation

--- Evaluation/getRating.py
+++ Evaluation/getRating.py
@@ -1,6 +1,17 @@
 import requests
 
 
+def extract_first_integer(sentence):
+    words = sentence.split()
+    for word in words:
+        try:
+            number = int(word)
+            return number
+        except ValueError:
+            continue
+    return 0
+
+
 def generate_text(prompt, api_key, endpoint, model):
     headers = {
         'Content-Type': 'application/json',
@@ -30,28 +41,129 @@ def generate_text(prompt, api_key, endpoint, model):
         return None
 
 
-def getRating(joke, prompt, api_key, endpoint, model='mistralai/Mixtral-8x7B-Instruct-v0.1'):
+def getFunnyScore(joke, api_key, endpoint, model):
     Instruction = """
-    Prompt: {}
+    INSTRUCTIONS: Given a joke, rate the joke based on the below rubric.
+    a. give score 2 if the joke is very funny.
+    b. give score 1 if the joke is slightly funny.
+    c. give score 0 if the joke is not funny.
+    
+    Only return the score value, do not give any explanation. See the examples below.
+    
+    Joke: [me narrating a documentary about narrators] I can't hear what they're saying cuz I'm talking.
+    Score: 2
+    
+    Joke: dark dark go away. 
+    Score: 1
+    
+    Joke: This is a joke about pencil.
+    Score: 0
+    
+    Generate a similar Score for the Joke I gave. Follow the rubric exactly.
+    Only give me the Score and nothing else, do not have 'Score: ' in it.
+    
     Joke: {} 
-    INSTRUCTIONS: Given a prompt and joke, give likert scale rating for it indicating how funny it is and how well it matches the joke. Give just the rating.
+    Score: {}
+    """.format(joke, '')
+
+    # Generate text
+    score = generate_text(Instruction, api_key, endpoint, model)
+    return extract_first_integer(score)
+
+
+def getCompleteScore(joke, api_key, endpoint, model):
+    Instruction = """
+    INSTRUCTIONS: Given a sentence, rate the sentence based on the below rubric.
+    a. give score 1 if the sentence is complete.
+    b. give score 0 if the sentence is not complete.
+    
+    Only return the score value, do not give any explanation. See the examples below.
+    
+    Sentence: This is a way to complete a sentence.
+    Score: 1
+    
+    Sentence: what did this 
+    Score: 0
+    
+    Generate a similar Score for the Sentence I gave. Follow the rubric exactly.
+    Only give me the Score and nothing else, do not have 'Score: ' in it.
+    
+    Sentence: {} 
+    Score: {}
+    """.format(joke, '')
+
+    # Generate text
+    score = generate_text(Instruction, api_key, endpoint, model)
+    return extract_first_integer(score)
+
+
+def getGrammarScore(joke, api_key, endpoint, model):
+    Instruction = """
+    INSTRUCTIONS: Given a sentence, rate the sentence based on the below rubric.
+    a. give score 1 if the sentence is grammatically correct.
+    b. give score 0 if the sentence is not grammatically correct.
+    
+    Only return the score value, do not give any explanation. See the examples below.
+    
+    Sentence: This is a grammatically correct sentence.
+    Score: 1
+    
+    Sentence: This are not one grammar correct. 
+    Score: 0
+    
+    Generate a similar Score for the Sentence I gave. Follow the rubric exactly.
+    Only give me the Score and nothing else, do not have 'Score: ' in it.
+    
+    Sentence: {} 
+    Score: {}
+    """.format(joke, '')
+
+    # Generate text
+    score = generate_text(Instruction, api_key, endpoint, model)
+    return extract_first_integer(score)
+
+
+def getPromptScore(joke, prompt, api_key, endpoint, model):
+    Instruction = """
+    INSTRUCTIONS: Given a prompt and joke, rate the joke based on the below rubric.
+    a. give score 2 if the joke matches the prompt perfectly.
+    b. give score 1 if the joke matches the prompt sufficiently.
+    c. give score 0 if the joke does not match the prompt.
+    
+    Only return the score value, do not give any explanation. See the examples below.
     
     Joke: [me narrating a documentary about narrators] I can't hear what they're saying cuz I'm talking
     Prompt: Tell me a joke about a documentary?
-    Rating: 3/5
+    Score: 2
     
-    Joke: Telling my daughter garlic is good for you. Good immune system and keeps pests away.Ticks, mosquitos, vampires... men.
-    Prompt: Give me a funny parent-child joke?
-    Rating: 4/5
+    Joke: is it light outside? 
+    Prompt: Give me a dark joke?
+    Score: 1
     
-    Generate a similar rating for the joke and prompt I gave. Only give me the Rating, do not have 'Rating: ' in it.
-    """.format(prompt, joke)
-
-    print(Instruction)
+    Joke: why did the chicken cross the road? it wanted to go to the other side.
+    Prompt: Give me a joke about a pencil?
+    Score: 0
+    
+    Generate a similar Score for the Joke and Prompt I gave. Follow the rubric exactly.
+    Only give me the Score and nothing else, do not have 'Score: ' in it.
+    
+    Prompt: {}
+    Joke: {} 
+    Score: {}
+    """.format(prompt, joke, '')
 
     # Generate text
-    generated_text = generate_text(Instruction, api_key, endpoint, model)
-    return generated_text
+    score = generate_text(Instruction, api_key, endpoint, model)
+    return extract_first_integer(score)
+
+
+def getRating(joke, prompt, api_key, endpoint, model='mistralai/Mixtral-8x7B-Instruct-v0.1'):
+    promptScore = getPromptScore(joke, prompt, api_key, endpoint, model)
+    funnyScore = getFunnyScore(joke, api_key, endpoint, model)
+    completeScore = getCompleteScore(joke, api_key, endpoint, model)
+    grammarScore = getGrammarScore(joke, api_key, endpoint, model)
+    return str(promptScore + funnyScore + completeScore + grammarScore)
+
 
 def getEval(prompt, preJoke, fineJoke, api_key, endpoint, model='mistralai/Mixtral-8x7B-Instruct-v0.1'):
     Instruction = """
---
+++ Evaluation/ratings.py
@@ -0,0 +1,27 @@
+from getRating import getRating
+import pandas as pd
+import numpy as np
+
+
+def loadRating(start, end):
+    df_total = pd.read_csv('../Evaluation/Jokes.csv')
+    df = df_total.iloc[start:end].copy()
+    prompts = df.iloc[:, 2].values
+    jokes = df.iloc[:, 1].values
+    jokes = jokes.flatten()
+    prompts = prompts.flatten()
+    ratings = []
+    api_key = '3f06bb7945192b6e58e9d4acd1d265fe51a1478015fb5cac5d67c3ca274b3c94'
+    endpoint = 'https://api.together.xyz/v1/chat/completions'
+    for i in range(len(jokes)):
+        joke = jokes[i]
+        prompt = prompts[i]
+        rating = getRating(joke, prompt, api_key, endpoint)
+        print(rating)
+        ratings.append(rating)
+
+    df['Rating'] = np.array(rating)
+    df.to_csv('ratings.csv', mode='a', header=False, index=False)
+
+
+loadRating(1, 50)
